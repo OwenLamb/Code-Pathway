73f40e2574dd61ec7d6e4873f40a3aca420a234f
OwenLamb <70539710+OwenLamb@users.noreply.github.com>
2024-11-24 19:03:46 -0500

    Finalized Lesson Plan
    I also moved all the unused files into the archive incase we still need them.

---
+++ src/scripts/Lesson3Script.js
@@ -0,0 +1,27 @@
+//Lesson1Script.js
+//This takes the data from the text box and saves it to a variable once the submit button has been pressed.
+var userQuizGrade = false;
+document.getElementById("Submit").addEventListener("click", function(){
+    textareaContent = document.getElementById("UserCode").value;
+
+    //Change to get AI's response on whether it works
+    //Update Users progress here too
+    if(textareaContent == "Passed"){
+        userQuizGrade = true;
+        document.getElementById('Continue').classList.remove('hidden');
+    } else {
+        alert("Incorrect Please Try Again");
+    }
+
+    let AIResponseDiv = document.getElementById('AIResponse');
+    AIResponseDiv.classList.remove('hidden')
+    AIResponseDiv.innerHTML = "Input AI's response here"
+})
+
+document.getElementById("Continue").addEventListener("click", function(){
+    if(userQuizGrade){
+        window.location.href = 'Lesson4.html';
+    } else {
+        alert("You have not yet passed the quiz")
+    }
+})
